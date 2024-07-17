f3f45a5ae9df0acc10a0a332cbe8419bd17d9e83
PSGSudalai <sudalaimuthug397@gmail.com>
2024-07-17 14:08:13 +0530

    yt

--- auth_app/views.py
+++ auth_app/views.py
@@ -39,7 +39,7 @@ def logout_view(request):
 
 def dashboard_view(request):
     searching = request.GET.get('search', '')
-    tags= Tags.objects.filter()
+    tags= Tags.objects.all()
     posts = PostModel.objects.filter((Q(title__icontains=searching)|Q(tags__tags__icontains=searching))).distinct()
     return render(request, 'dashboard.html', {'posts': posts,'tags':tags})
 
